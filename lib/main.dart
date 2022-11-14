7e06b9067c99f7f5d068abec3a7fb3ea9ff8c000
rukky96 <105091321+rukky96@users.noreply.github.com>
2022-11-14 12:31:02 +0100

    Corrected Problems

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'Poppins'),
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
@@ -45,75 +46,198 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
-              height: 150,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                border: Border.all(width: 0, color: Colors.transparent),
-                color: Colors.blue.shade400,
-              ),
-              child: Container(
-                padding: const EdgeInsets.all(35),
+            Stack(children: [
+              Container(
                 height: 150,
                 width: double.infinity,
                 decoration: BoxDecoration(
-                  border: Border.all(width: 0),
-                  borderRadius: const BorderRadius.only(
-                      bottomRight: Radius.elliptical(100, 75)),
-                  color: Colors.black,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text(
-                      'Hello',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'Daniel',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 40,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Container(
-              height: 120,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                border: Border.all(width: 0),
-                // color: Colors.black,
-              ),
-              child: Container(
-                height: 120,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  border: Border.all(width: 0),
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.elliptical(100, 90),
-                  ),
-                  color: Colors.deepPurple.shade400,
+                  border: Border.all(width: 0, color: Colors.transparent),
+                  color: Colors.blue.shade400,
                 ),
                 child: Container(
-                    padding: const EdgeInsets.only(right: 50),
-                    height: 122,
+                  padding: const EdgeInsets.all(35),
+                  height: 150,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 0),
+                    borderRadius: const BorderRadius.only(
+                        bottomRight: Radius.elliptical(100, 75)),
+                    color: Colors.black,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Hello',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Daniel',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ]),
+            Stack(
+              children: [
+                Container(
+                  height: 120,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 0),
+                    color: Colors.black,
+                  ),
+                  child: Container(
+                    height: 120,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 0),
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.elliptical(100, 90),
+                      ),
+                      color: Colors.deepPurple.shade400,
+                    ),
+                    child: Container(
+                        padding: const EdgeInsets.only(right: 50),
+                        height: 122,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.only(
+                              topLeft: Radius.elliptical(100, 90),
+                              bottomRight: Radius.elliptical(100, 75)),
+                          color: Colors.blue.shade400,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => SecondPage(
+                                              selectedIndex: 2,
+                                            )));
+                              },
+                              child: const Text(
+                                'Gym',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 50,
+                            ),
+                            const Text(
+                              '30',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        )),
+                  ),
+                ),
+              ],
+            ),
+            Stack(
+              children: [
+                Container(
+                  height: 120,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.blue.shade400,
+                  ),
+                  child: Container(
+                    height: 120,
                     width: double.infinity,
                     decoration: BoxDecoration(
                       borderRadius: const BorderRadius.only(
-                          topLeft: Radius.elliptical(100, 90),
-                          bottomRight: Radius.elliptical(100, 75)),
-                      color: Colors.blue.shade400,
+                        topLeft: Radius.elliptical(100, 90),
+                      ),
+                      color: Colors.blue.shade900,
+                    ),
+                    child: Container(
+                        padding: const EdgeInsets.only(right: 50),
+                        height: 120,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.only(
+                              topLeft: Radius.elliptical(100, 90),
+                              bottomRight: Radius.elliptical(100, 75)),
+                          color: Colors.deepPurple.shade400,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => SecondPage(
+                                              selectedIndex: 1,
+                                            )));
+                              },
+                              child: const Text(
+                                'Work',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 50,
+                            ),
+                            const Text(
+                              '15',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        )),
+                  ),
+                ),
+              ],
+            ),
+            Stack(
+              children: [
+                Container(
+                  height: 400,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple.shade400,
+                  ),
+                  child: Container(
+                    padding: const EdgeInsets.fromLTRB(30, 50, 50, 30),
+                    height: 400,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.elliptical(100, 90),
+                      ),
+                      color: Colors.blue.shade900,
                     ),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         TextButton(
                           onPressed: () {
@@ -121,11 +245,11 @@ class _MyHomePageState extends State<MyHomePage> {
                                 context,
                                 MaterialPageRoute(
                                     builder: (context) => SecondPage(
-                                          selectedIndex: 2,
+                                          selectedIndex: 0,
                                         )));
                           },
                           child: const Text(
-                            'Gym',
+                            'Home',
                             style: TextStyle(
                                 color: Colors.white,
                                 fontSize: 25,
@@ -143,120 +267,11 @@ class _MyHomePageState extends State<MyHomePage> {
                               fontWeight: FontWeight.bold),
                         ),
                       ],
-                    )),
-              ),
-            ),
-            Container(
-              height: 120,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.blue.shade400,
-              ),
-              child: Container(
-                height: 120,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.elliptical(100, 90),
+                    ),
                   ),
-                  color: Colors.blue.shade900,
                 ),
-                child: Container(
-                    padding: const EdgeInsets.only(right: 50),
-                    height: 120,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.only(
-                          topLeft: Radius.elliptical(100, 90),
-                          bottomRight: Radius.elliptical(100, 75)),
-                      color: Colors.deepPurple.shade400,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => SecondPage(
-                                          selectedIndex: 1,
-                                        )));
-                          },
-                          child: const Text(
-                            'Work',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 25,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 50,
-                        ),
-                        const Text(
-                          '15',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    )),
-              ),
-            ),
-            Container(
-              height: 400,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.deepPurple.shade400,
-              ),
-              child: Container(
-                padding: const EdgeInsets.fromLTRB(30, 50, 50, 30),
-                height: 400,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.elliptical(100, 90),
-                  ),
-                  color: Colors.blue.shade900,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => SecondPage(
-                                      selectedIndex: 0,
-                                    )));
-                      },
-                      child: const Text(
-                        'Home',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 50,
-                    ),
-                    const Text(
-                      '30',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+              ],
+            )
           ],
         ),
       ),
